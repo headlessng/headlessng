ce24fe50f47035cc352a547f829b45349da42a37
Adam Wojtaś <wojtas.adam@hotmail.com>
2024-08-20 09:55:15 +0200

    added favicon and fix lint

--- applications/headlessng-playground/src/app/app.config.ts
+++ applications/headlessng-playground/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+
+import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
